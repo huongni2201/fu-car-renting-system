3ff913a7cb2d96450da68bbd562b5f52c50b6ffe
HuongNI <huongnn2201@gmail.com>
2026-07-07 21:37:00 +0700

    Update login

---
+++ services/customer-service/src/main/java/com/example/customerservice/mapper/CustomerMapper.java
@@ -0,0 +1,19 @@
+package com.example.customerservice.mapper;
+
+import com.example.customerservice.domain.entity.Customer;
+import com.example.customerservice.dto.response.CustomerResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+
+  public CustomerResponse toCustomerResponse(Customer customer) {
+    return CustomerResponse.builder()
+        .customerName(customer.getCustomerName())
+        .telephone(customer.getTelephone())
+        .email(customer.getEmail())
+        .customerBirthday(customer.getCustomerBirthday())
+        .customerStatus(customer.getCustomerStatus().name())
+        .build();
+  }
+}
